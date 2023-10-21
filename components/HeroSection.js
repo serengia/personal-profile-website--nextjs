88195490ecadf4dc583aef934642b1132601efba
James Serengia <jamesserengia@gmail.com>
2023-10-21 16:17:00 +0300

    Update welcome hero text

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -19,19 +19,17 @@ function HeroSection() {
     <section className="section-hero">
       <div className="row hero-content">
         <div className="header-text">
-          <h1 className="heading-primary">
-            Hey there, my name is James Serengia.
-          </h1>
+          <h1 className="heading-primary">Hello, my name is James Serengia</h1>
           <h2 className="heading-secondary heading-secondary--green">
-            A software Engineer based in Nairobi Kenya
+            A Full-stack Software Engineer based in Nairobi Kenya
           </h2>
         </div>
         <p className="description-text">
-          I am a Software Engineer with 6+ years of experience. With my wide
-          range of experiences, I can help you build a product, feature or
-          website. Look through some of my work and experiences! If you like
-          what you see and have a project you need coded, don’t hesitate to
-          contact me.
+          I am a Senior Software Engineer with 6+ years of experience. With my
+          wide range of experiences, I would love to help you build your next
+          big software product, feature, web application or a basic website.
+          Look through some of my work and experiences! If you like what you see
+          and have a project you need coded, don’t hesitate to reach out🚀🚀
         </p>
         <div className="hero-btn-wrapper">
           <button
